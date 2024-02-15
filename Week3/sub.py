8238ac2b01c6bac3b2f8b08b607814658d1d0c84
Grainne10 <grainne_boyle@outlook.com>
2024-02-15 17:20:22 +0000

    Create subtraction file and amend testTypes file

---
+++ Week3/sub.py
@@ -0,0 +1,8 @@
+# sub.py  
+# Program reads two numbers and subtracts one from the other
+# author Grainne Boyle
+
+x = int(input("Enter fist number: "))
+y = int(input("Enter second number:"))
+answer = x-y
+print("{} minus {} is {} ".format (x, y, answer))
