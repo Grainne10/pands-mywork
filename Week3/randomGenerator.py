452e7052005f668bd896238d6fb3c3c3faf8884a
Grainne10 <grainne_boyle@outlook.com>
2024-02-16 16:19:23 +0000

    randomGenerator file

---
+++ Week3/randomGenerator.py
@@ -0,0 +1,7 @@
+# randomGenerator.py  
+# Program prints out a random number between1 and 150
+# author Grainne Boyle
+
+import random 
+number = random.randint(1,150)
+print("here is a random number {}".format(number))
