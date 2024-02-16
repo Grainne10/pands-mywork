120c668708d4f5161046e5b60aa64b0e286d738b
Grainne10 <grainne_boyle@outlook.com>
2024-02-16 16:10:48 +0000

    Created Div.py file

---
+++ Week3/div.py
@@ -0,0 +1,9 @@
+# div.py  
+# Program reads two numbers and divides the first number by the second and gives the integer result and the remainder
+# author Grainne Boyle
+
+x = int(input("Enter first number: "))
+y = int(input("Enter the number you want to divide by: "))
+answer = int(x//y) # // gives the int division
+remainder = int(x%y) # % gives the remainder
+print ("{} divided by {} is {} with remainder {}".format( x, y, answer, remainder))
