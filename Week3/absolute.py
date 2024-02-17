f16e14af14c4a903bc7d47077349937964004a3d
Grainne10 <grainne_boyle@outlook.com>
2024-02-17 12:31:48 +0000

    Upload absolute and round files

---
+++ Week3/absolute.py
@@ -0,0 +1,8 @@
+# absolute.py  
+# Program gives the absolute value of a number
+# author Grainne Boyle
+# question is in decimal so presume that floats should be used
+
+number = float(input("Enter a number: "))
+absoluteValue = abs(number)
+print ('The absolute value of {} is {} '.format(number, absoluteValue))
---
+++ Week3/round.py
@@ -0,0 +1,7 @@
+# round.py  
+# Program rounds a number
+# author Grainne Boyle
+
+numbertoRound = float(input("Enter a float number"))
+roundedNumber = round(numbertoRound)
+print ('{} rounded is{}' .format(numbertoRound,roundedNumber))
